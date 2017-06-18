d0a002958f865a73d6d4466b7731675a1da50450
Zhenyu-Liu <liu.4706@osu.edu>
2017-06-17 21:03:36 -0400

    Solve Backwash with two UF object

--- Programming Assignment/Percolation/src/Percolation.java
+++ Programming Assignment/Percolation/src/Percolation.java
@@ -1,36 +1,82 @@
+/*--------------------------------------------------------------------
+ * Author Zhenyu Liu
+ * Date: 6/16/17
+ *
+ * Purpose: Learn to use just WeightedQuickUnionUF and monte carlo to
+ * solve Percolation problem. As for this Percolation class we construct
+ * a n * n grid and implement several essential methods to simulate the
+ * situation.
+ *
+ * Compilation: javac Percolation.java
+ * Test via PercolationVisualizer
+ * Execution: PercolationVisualizer input20.txt
+ * The result will shows on the java application
+ --------------------------------------------------------------------*/
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-/**
- * @author Zhenyu Liu
- *
- * Date: 6/16/17
- */
 public class Percolation {
 
-    private WeightedQuickUnionUF siteMatrix;
+    private WeightedQuickUnionUF mainGrid, grid; // Use two UF object for solve backwash
 
-    private int size;
+    private int size; // size of grid
 
-    private boolean[] openStatus;
+    private boolean[] openStatus; // true if site is open
 
-    private int numberOfOpenSite;
+    private int numberOfOpenSite; // track number of opened site
 
+    /**
+     * Constructor: Initial two grid mainGrid will contain both top
+     * and bottom visual site, and grid will only have top visual site
+     *
+     * @param n size of the grid
+     */
+    public Percolation(int n) {
 
-    private int xyToID (int row, int col){
-        return (row - 1)*size + col;
+        if (n <= 0) throw new IllegalArgumentException("size of grid need greater than 0");
+
+        mainGrid = new WeightedQuickUnionUF(n * n + 2);
+        grid = new WeightedQuickUnionUF(n * n + 1);
+        size = n;
+        numberOfOpenSite = 0;
+        openStatus = new boolean[n * n + 1];
     }
 
-    private boolean isVaild(int row, int col){
+    /**
+     * Convert 2D matrix into 1D number
+     *
+     * @param row: interval [1, size]
+     * @param col  interval [1, size]
+     * @return the index that in UF object and openSite array
+     */
+    private int xyToID(int row, int col) {
+
+        return (row - 1) * size + col;
+    }
+
+    /**
+     * Check if input row and col number is in the size of grid
+     *
+     * @param row row number
+     * @param col col number
+     * @return true if row and col number is within the size
+     * of grid (size * size)
+     */
+    private boolean isVaild(int row, int col) {
 
         if (row <= 0 || row > size) return false;
         if (col <= 0 || col > size) return false;
 
-        int idx = xyToID(row, col);
+        int idx = xyToID(row, col);    // Index in UF object
         return idx >= 1 && idx <= size * size;
     }
 
-    // check the open status around this index connect the union find object if found
-    private void merge(int idx){
+    /**
+     * Union all adjacent open site for current open site
+     *
+     * @param idx index of current site in UF object
+     */
+    private void merge(int idx) {
 
         // Adjacent site index
         int top = idx - size;
@@ -39,100 +85,114 @@ public class Percolation {
         int right = idx + 1;
 
         // union adjacent site if there is a open
-        if (top > 0  && openStatus[top]) siteMatrix.union(idx, top);
-        if (bottom <= size*size && openStatus[bottom]) siteMatrix.union(idx, bottom);
-        if ((idx - 1) % size != 0 && openStatus[left]) siteMatrix.union(idx, left);
-        if (idx % size != 0 && openStatus[right]) siteMatrix.union(idx, right);
-
-    }
-
-    public Percolation (int n){
-
-        /*
-         * create 1-dimensional union find object [1, n^2],
-         * with two visual-site 0 and n^2 + 1
-        */
-        siteMatrix = new WeightedQuickUnionUF(n * n + 2);
-        size = n;
-        numberOfOpenSite = 0;
-        // Align with the index of UF object [1, n*n]
-        openStatus = new boolean[n * n + 1];
-
-        // connect top & bottom visual site with top and bottom row
-
-        // top row
-        for (int i = 1; i <= n; i++){
-            siteMatrix.union(0, i);
+        if (top > 0 && openStatus[top]) {
+            mainGrid.union(idx, top);
+            grid.union(idx, top);
         }
-
-        // bottom row
-        for (int i = xyToID(size, 1); i <= n * n; i++){
-            siteMatrix.union(n * n + 1, i);
+        if (bottom <= size * size && openStatus[bottom]) {
+            mainGrid.union(idx, bottom);
+            grid.union(idx, bottom);
+        }
+        if ((idx - 1) % size != 0 && openStatus[left]) {
+            mainGrid.union(idx, left);
+            grid.union(idx, left);
+        }
+        if (idx % size != 0 && openStatus[right]) {
+            mainGrid.union(idx, right);
+            grid.union(idx, right);
         }
 
     }
 
-    public void open(int row, int col){
+    /**
+     * Open current site and union this site with all adjacent site.
+     * handle special case for top and bottom row
+     * For the grid UF object, does not have bottom visual site
+     *
+     * @param row row number
+     * @param col col number
+     */
+    public void open(int row, int col) {
 
         int idx = xyToID(row, col);
 
         if (!isVaild(row, col)) throw new IndexOutOfBoundsException("index out of bound");
 
-        if (!openStatus[idx]){
+        if (!openStatus[idx]) {
             openStatus[idx] = true;
             numberOfOpenSite++;
             merge(idx);
+
+            if (row == 1) {
+                mainGrid.union(idx, 0);
+                grid.union(idx, 0);
+            }
+
+            if (row == size) {
+                mainGrid.union(idx, size * size + 1);
+            }
         }
     }
 
-
-    public boolean isOpen(int row, int col){
+    /**
+     * Check current site is open
+     *
+     * @param row row number
+     * @param col col number
+     * @return true if site is open
+     */
+    public boolean isOpen(int row, int col) {
 
         if (!isVaild(row, col))
             throw new IndexOutOfBoundsException("index out of bound");
 
         return openStatus[xyToID(row, col)];
     }
-    /*
+
+    /*----------------------------------------------------------------------------------------
      * A full site is an open site that can be connected to an open site in the top row via a
      * chain of neighboring (left, right, up, down) open sites. If there is a full site in
      * the bottom row, then we say that the system percolates.
+     ---------------------------------------------------------------------------------------*/
+
+    /**
+     * Check if current site is connected with the top visual site
+     *
+     * @param row row number
+     * @param col col number
+     * @return true if current site has connection with top visual site
      */
-    public boolean isFull(int row, int col){
+    public boolean isFull(int row, int col) {
 
         if (!isVaild(row, col))
             throw new IndexOutOfBoundsException("index out of bound");
 
-        if ((row == 1 || row == size) && !isOpen(row, col)) {
-            return false;
-        }
-
-        return siteMatrix.connected(0, xyToID(row, col));
+        int idx = xyToID(row, col);
+        /*
+         * Key to avoid backwash, check both mainGrid (contain both top&bottom visual site)
+         * and grid (only contain the top) if they both have connection to the top, then it
+         * is full. Because in the grid, last row site does not have connection with bottom
+         * visual site
+         */
+        return mainGrid.connected(0, idx) && grid.connected(0, idx);
     }
 
-    public int numberOfOpenSites(){
-
+    /**
+     * Keep track the number of opened sites in the mainGrid
+     *
+     * @return number of opened sites
+     */
+    public int numberOfOpenSites() {
         return numberOfOpenSite;
     }
 
-    public boolean percolates(){
-        return siteMatrix.connected(0, size * size + 1);
-    }
-
-    public static void main(String[] args){
-
-        Percolation A = new Percolation(4);
-
-        A.open(1, 4);
-        A.open(2, 4);
-        A.open(2, 3);
-        A.open(3,3);
-        // A.open(4, 3);
-
-        // System.out.println(A.openStatus.length);
-
-        System.out.println(A.numberOfOpenSite);
-        // System.out.println(A.siteMatrix.connected(17, 15));
+    /**
+     * Check the if the grid is percolate
+     *
+     * @return true if the grid is percolate
+     */
+    public boolean percolates() {
+        return mainGrid.connected(0, size * size + 1);
     }
 
 }
--- Programming Assignment/Percolation/src/PercolationStats.java
+++ Programming Assignment/Percolation/src/PercolationStats.java
@@ -1,5 +1,98 @@
-/**
- * Created by liuzhenyu on 6/16/17.
- */
+/*----------------------------------------------------------------
+ *
+ * Author: Zhenyu Liu
+ * Written: 6/17/2017
+ *
+ * Compilation: javac PercolationStats.java
+ * Execution:   java PelcolationStats size trials
+ *
+ * Prints "mean:
+ *         stddev:
+ *         95% confidence interval"
+ *
+ ----------------------------------------------------------------*/
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+
+    private Percolation matrix;
+    // keep every independent trails
+    private double[] threshold;
+
+    private int numTrials;
+
+    /**
+     * Constructor: initial n * n size grid, and iterate number of trials
+     * independent percolate test. Then keep all threshold result.
+     * @param n: size of the Percolation grid
+     * @param trials: number of trials
+     */
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException("size of grid and trials must great than 0");
+
+        numTrials = trials;
+        threshold = new double[trials];
+
+        // Count number of trials
+        for (int i = 0; i < trials; i++) {
+            matrix = new Percolation(n);
+
+
+            while (!matrix.percolates()) {
+                // Randomly choose row and col number to open
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+
+                if (matrix.isOpen(row, col)) continue;
+
+                matrix.open(row, col);
+            }
+
+            threshold[i] = matrix.numberOfOpenSites() / (double) (n * n);
+        }
+
+
+    }
+
+    /**
+     * Calculate mean
+     * @return mean amount all threshold result
+     */
+    public double mean() { return StdStats.mean(threshold); }
+
+    /**
+     * Calculate stand division
+     * @return stand division amount all threshold result
+     */
+    public double stddev() { return StdStats.stddev(threshold); }
+
+    /**
+     * Calculate low bound of confidence interval
+     * @return low bound of confidence interval
+     */
+    public double confidenceLo() { return mean() - 1.96 * stddev() / Math.sqrt(numTrials); }
+
+    /**
+     * Calculate high bound of confidence interval
+     * @return high bound of confidence interval
+     */
+    public double confidenceHi() { return mean() + 1.96 * stddev() / Math.sqrt(numTrials); }
+
+
+    public static void main(String[] args) {
+
+        int n =  StdIn.readInt();
+        int trial = StdIn.readInt();
+        PercolationStats stat = new PercolationStats(n, trial);
+
+        StdOut.println("mean \t = " + stat.mean());
+        StdOut.println("stddev \t = " + stat.stddev());
+        StdOut.printf("95%% confidence interval \t = [%f, %f]",
+                stat.confidenceLo(), stat.confidenceHi());
+    }
 }
